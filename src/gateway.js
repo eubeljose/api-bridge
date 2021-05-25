cae462b3a62ec46fb400734ce90f498ecb7071cb
Bots.Business <git.sync@bots.business>
2021-05-25 12:55:54 +1000

    fix Gateway class

--- src/gateway.js
+++ src/gateway.js
@@ -1,26 +1,25 @@
-var Validator = exports,
-               init
-
-const { pathToRegexp, match, parse, compile } = require("path-to-regexp");
-
-
-Validator.init = function(){
-  console.log( haveMethod("api.bots.business/bots/4454?ddddff") )
-}
-
-function haveMethod(url){
-  
-  if(!url){ return }
-  if(url==""){ return }
-
-  var fn = match("api.bots.business/bots/:id");
-
-  // "api.bots.business/bots/22?any_query=true"
-  url = url.split('?')[0]
-
-  test = fn(url)
-  if(test){
-    return true
+class Gateway{
+  constructor(){
   }
-  return false
+
+  haveMethod(url){
+    if(!url){ return }
+    if(url==""){ return }
+
+    let { pathToRegexp, match, parse, compile } = require("path-to-regexp");
+
+    var fn = match("api.bots.business/bots/:id");
+
+    // "api.bots.business/bots/22?any_query=true"
+    url = url.split('?')[0]
+
+    let test = fn(url)
+    if(test){
+      return true
+    }
+    return false
+  }
+
 }
+
+module.exports = Gateway;
