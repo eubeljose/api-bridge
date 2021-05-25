fb31917ccdfce5c9c019777001f6c5963dcaa2bf
Bots.Business <git.sync@bots.business>
2021-05-25 15:32:06 +1000

    rework for erros; can parse safe params

--- src/app.test.js
+++ src/app.test.js
@@ -9,6 +9,12 @@ describe("Unit Test", function() {
         let policy = gateway.policies[0]
         assert.strictEqual(policy.name, "BBApi");
         assert.strictEqual(policy.api_url, "https://api.bots.business/v1");
+        assert.strictEqual(policy.safe_params[0], { api_key: "" } );
+
+        assert.strictEqual(policy.methods.length, 3);
+        assert.strictEqual(policy.methods[0],
+          { path: "/bots", needAsk: false, method: "GET" }
+        );
     });
 
     it("validate example urls", function() {
--- src/policy.js
+++ src/policy.js
@@ -7,16 +7,19 @@ class Policy{
     }
 
     this.name = fileName;
+    this.safe_params = [];
     this._parseContent(content);
   }
 
   _parseContent(content){
     let lines = content.split("\n");
     let line;
-    for(let ind in lines){
-      line = lines[ind];
+    this.curLine = 0;
+    for(this.curLine in lines){
+      line = lines[this.curLine];
       if(this._isComment(line)){ continue }
       if(this._parseApiUrl(line)){ continue }
+      if(this._parseSafeParam(line)){ continue }
     }
   }
 
@@ -26,8 +29,13 @@ class Policy{
     if(text == ""){ return true }
   }
 
+  _throwError(title){
+    let err = title + " Policy: " + this.name + ". Line: " + String(this.curLine + 1);
+    throw new Error(err);
+  }
+
   _isComment(line){
-    return (line.split(" ").join("")[0] == "#")  // it is comment
+    return (line.split(" ").join("")[0] == "#")
   }
 
   _parseApiUrl(line){
@@ -37,17 +45,52 @@ class Policy{
     }
 
     if(this.api_url){
-      throw new Error("Can not set Api url twice. Policy: " + this.name)
+      this._throwError("Can not set Api url twice.")
     }
 
-    if(this._isBlank(arr[1])){
-      throw new Error("Syntax error in policy. No API url. Policy: " + this.name)
+    let url = arr[1];
+
+    if(this._isBlank(url)){
+      this._throwError("No API url.")
     }
 
-    this.api_url = arr[1];
+    if(url.split('http')[0]!=''){
+      url = 'http://' + url
+    }
+
+    this.api_url = url;
     return true;
   }
 
-}
 
+  _isSafeParam(arr){
+    return ((arr[0] == "SAFE")&&((arr[1] == "PARAM")))
+  }
+
+  _parseSafeParam(line){
+    let arr = line.split(' ');
+    if(!this._isSafeParam(arr)){ return }
+
+    let keys = arr[2];
+
+    if(this._isBlank(keys)){
+      this._throwError("No safe param. Need key_name=ENV.")
+    }
+      
+    let arr_params = arr[2].split("=");
+    if(!arr_params[0]){
+      this._throwError("Invalid safe param. Need key_name like key_name=ENV.")
+    }
+
+    if(!arr_params[1]){
+      this._throwError("Invalid safe param. Need ENV like key_name=ENV.")
+    }
+
+    if(!process.env[arr_params[2]]){
+      this._throwError("Need set ENV value for " + arr_params[1])
+    }
+
+  }
+
+}
 module.exports = Policy;
